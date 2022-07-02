95cb9f434494c2fb3641d44deabb95df7a492850
Lart <tlartigau.pro@gmail.com>
2022-07-03 01:09:30 +0200

    fix: wasn't working properly on sunday
    The Date.getDay() method is zero-indexed on sunday by default
    Sunday index is now set to 7 so the function works as expected

--- frontend/src/utils/logic/index.ts
+++ frontend/src/utils/logic/index.ts
@@ -3,7 +3,8 @@ const MS_IN_A_DAY = 1000 * 60 * 60 * 24;
 export const getFirstDayOfWeek = (offset: number) => {
     const today = new Date(Date.now() + MS_IN_A_DAY * 7 * offset);
     const todayNumber = today.getDay();
-    const deltaFromMonday = todayNumber - 1;
+    // days are zero-indexed on sunday by delfault but we're Europeans here
+    const deltaFromMonday = todayNumber === 0 ? 6 : todayNumber - 1;
     if (deltaFromMonday === 0) {
         return today
     }
