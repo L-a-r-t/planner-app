c0e595f286eacdff9308c9f5b30f6bb042354c4e
Lart <tlartigau.pro@gmail.com>
2022-07-08 17:34:05 +0200

    feat: add 'owner' boolean in http res
    to be used in the browser to for conditionnal UI rendering

--- backend/utils/checkUserCalendar.ts
+++ backend/utils/checkUserCalendar.ts
@@ -20,16 +20,19 @@ type Calendar = mongoose.Document<unknown, any, {
     _id: any
 }
 
-const checkUserCalendar = async (calendar: Calendar, email: string) => {
+type match = [boolean, boolean]
+
+const checkUserCalendar = async (calendar: Calendar, email: string): Promise<match> => {
+    let user: any
     if (!calendar.public) {
         if (!calendar.access?.some(elem => {
                 const parsed = elem as any
                 return parsed.email === email
             })) {
-            return false
+            return [false, false]
         }
-        const user = await User.findOne({ email })
-        if (!user?.calendars?.some(elem => {
+        user = await User.findOne({ email })
+        if (!user?.calendars?.some((elem: unknown) => {
             const parsed = elem as any
             return parsed._id.toString() === calendar._id.toString()
         })) {
@@ -38,8 +41,12 @@ const checkUserCalendar = async (calendar: Calendar, email: string) => {
                 await user?.save()
             }
         }
+    } else {
+        user = await User.findOne({ email })
     }
-    return true
+    const owner = calendar.owner as any
+    const isOwner = owner.toString() === user._id.toString();
+    return [true, isOwner]
 }
 
 export default checkUserCalendar
