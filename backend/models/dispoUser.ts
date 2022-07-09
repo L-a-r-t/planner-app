1d8d050c1bbf261a5362c4970b3c398b56a24ea4
Lart <tlartigau.pro@gmail.com>
2022-07-10 00:20:34 +0200

    fix: user Model & function, port
    user model's list of calendars wasn't set properly for population
    checkUserCalendar util function wasn't updating user doc everytime

--- backend/models/dispoUser.ts
+++ backend/models/dispoUser.ts
@@ -1,12 +1,5 @@
 import mongoose = require('mongoose')
 
-const calendarID = new mongoose.Schema({
-    id: {
-        type: mongoose.Types.ObjectId,
-        ref: "Calendar"
-    }
-})
-
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -18,7 +11,7 @@ const userSchema = new mongoose.Schema({
         match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g, 'Please fill a valid email address']
     },
     verified: Boolean,
-    calendars: [calendarID]
+    calendars: [{type: 'ObjectId'}]
 })
 
 const User = mongoose.model('CalendarUser', userSchema)
--- backend/utils/checkUserCalendar.ts
+++ backend/utils/checkUserCalendar.ts
@@ -23,7 +23,6 @@ type Calendar = mongoose.Document<unknown, any, {
 type match = [boolean, boolean]
 
 const checkUserCalendar = async (calendar: Calendar, email: string): Promise<match> => {
-    let user: any
     if (!calendar.public) {
         if (!calendar.access?.some(elem => {
                 const parsed = elem as any
@@ -31,21 +30,19 @@ const checkUserCalendar = async (calendar: Calendar, email: string): Promise<mat
             })) {
             return [false, false]
         }
-        user = await User.findOne({ email })
-        if (!user?.calendars?.some((elem: unknown) => {
-            const parsed = elem as any
-            return parsed._id.toString() === calendar._id.toString()
-        })) {
-            if (user && user.calendars) {
-                user.calendars.push(calendar._id);
-                await user?.save()
-            }
+    } 
+    const user = await User.findOne({ email })
+    if (!user?.calendars?.some((elem: unknown) => {
+        const parsed = elem as any
+        return parsed._id.toString() === calendar._id.toString()
+    })) {
+        if (user && user.calendars) {
+            user.calendars.push(calendar._id);
+            await user?.save()
         }
-    } else {
-        user = await User.findOne({ email })
     }
     const owner = calendar.owner as any
-    const isOwner = owner.toString() === user._id.toString();
+    const isOwner = owner.toString() === user?._id.toString();
     return [true, isOwner]
 }
 
